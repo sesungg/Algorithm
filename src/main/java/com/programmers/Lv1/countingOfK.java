517aae32133be67e12b88e4d1a1930576f8573f5
sesungg <sesungg@4depth.com>
2023-07-10 22:10:40 +0900

    K의 개수

---
+++ src/main/java/com/programmers/Lv1/countingOfK.java
@@ -0,0 +1,37 @@
+package com.programmers.Lv1;
+
+public class countingOfK {
+    static class Solution {
+        public int solution(int startNum, int endNum, int checkNum){
+            int answer = 0;
+            int current = 0;
+            int digit = 0;
+
+            for (int i = startNum; i <= endNum; i++) {
+                current = i;
+
+                while (current > 0) {
+                    digit = current % 10;
+                    if (digit == checkNum) {
+                        answer++;
+                    }
+                    current /= 10;
+                }
+            }
+
+            return answer;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int firstResult = solution.solution(1, 13, 1);
+        System.out.println("startNumber : 1");
+        System.out.println("endNumber : 13");
+        System.out.println("checkNumber : 1");
+
+        if (firstResult == 6) {
+            System.out.println("success");
+        }
+    }
+}
