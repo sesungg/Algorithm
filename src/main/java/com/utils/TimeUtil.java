8599a16355de215c0ec74b2ca25004fca04b540a
sesungg <sesungg@gmail.com>
2024-06-17 11:06:30 +0900

    버블 정렬

--- src/main/java/com/utils/TimeUtil.java
+++ src/main/java/com/utils/TimeUtil.java
@@ -9,12 +9,12 @@ public class TimeUtil {
         startTime = System.nanoTime();
     }
 
-    public String getElapsedTimeInSeconds() {
+    public void getElapsedTimeInSeconds() {
         long endTime = System.nanoTime();
         // 나노초를 초 단위로 변환.
         double elapsedTimeInSeconds = (endTime - startTime) / 1_000_000_000.0;
         // 소수점 다섯 번째 자리까지 반올림하여 반환.
         DecimalFormat df = new DecimalFormat("0.00000");
-        return df.format(elapsedTimeInSeconds);
+        System.out.println("소요시간 : " + df.format(elapsedTimeInSeconds));
     }
 }
